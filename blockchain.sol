4b5e21b9a2f8076e107c410a9aa7430551990220
Stephane <azerot.stephane@gmail.com>
2020-12-02 17:46:55 +0100

    smart contract

---
+++ blockchain.sol
@@ -0,0 +1,26 @@
+pragma solidity >0.4.99;
+
+contract Trading{
+    uint8 public amountInitial=100;
+    uint256 timer=3600;
+    address payable[] public players;
+    struct Player {
+      uint256 amountBet;
+
+   }
+
+    function createCompetition(uint8 _amountInitial) public {
+
+
+    }
+    function runCompetition() public {
+
+    }
+    function getResults() public {
+
+    }
+    function getWinPrice() view public returns (uint256) {
+
+    }
+
+}
