df2cd62bfd1ce5e53d42fc6fe0ee7ab165b45d37
aassali <assali.antoine@gmail.com>
2021-01-08 12:14:53 +0100

    Edit framework components

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './framework-components/header/header.component';
+import { FooterComponent } from './framework-components/footer/footer.component';
+import { HomeComponent } from './project-components/home/home.component';
+import { ShareHouseComponent } from './project-components/share-house/share-house.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShareHouseComponent
   ],
   imports: [
     BrowserModule,
